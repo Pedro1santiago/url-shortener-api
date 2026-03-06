955315a15d1306b70994533bf84e58de79eb65db
Pedro1santiago <pedro.santigosiqueira@gmail.com>
2026-03-06 02:24:11 -0300

    Add security config and improve short URL creation logic
    - Configure Spring Security and CORS
    - Refactor ShortUrlService to handle unique codes via database constraint
    - Improve URL validation

--- src/main/java/urlshortener/util/ShortCodeGenerator.java
+++ src/main/java/urlshortener/util/ShortCodeGenerator.java
@@ -4,21 +4,19 @@ import java.security.SecureRandom;
 
 public final class ShortCodeGenerator {
 
-    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String BASE62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     private static final SecureRandom RANDOM = new SecureRandom();
 
-    private ShortCodeGenerator() {
-    }
+    private ShortCodeGenerator() {}
 
+    public static String generateCode(int length) {
 
-    public static String generateCode(int length){
+        StringBuilder code = new StringBuilder(length);
 
-        StringBuilder code = new StringBuilder();
-
-        for(int i = 0; i < length; i++){
-            int index = RANDOM.nextInt(CHARACTERS.length());
-            code.append(CHARACTERS.charAt(index));
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(BASE62.length());
+            code.append(BASE62.charAt(index));
         }
 
         return code.toString();
